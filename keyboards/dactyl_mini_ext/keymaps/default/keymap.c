0eeb161c9cc57654c58305a9135a79952c8c69ba
jinoralen <yura.khariton@gmail.com>
2023-03-08 12:45:16 +0200

    removed unused layer; updated sofle keymap;

--- keyboards/dactyl_mini_ext/keymaps/default/keymap.c
+++ keyboards/dactyl_mini_ext/keymaps/default/keymap.c
@@ -4,7 +4,6 @@
 
 #define _SYMR 1
 #define _SYML 2
-#define _NUM 3
 
 #define _MEDIA 4
 
@@ -32,151 +31,108 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = KEYMAP(
-	KC_GRV  , KC_Q   , KC_W    , KC_E    , KC_R      , KC_T                           , KC_Y       , KC_U      , KC_I   , KC_O   , KC_P     , KC_LBRC , 
-      KC_DEL  , KC_A   , KC_S    , KC_D    , KC_F      , KC_G                           , KC_H       , KC_J      , KC_K   , KC_L   , KC_SCLN  , KC_QUOT , 
-      MT_TAB  , KC_Z   , KC_X    , KC_C    , KC_V      , KC_B                           , KC_N       , KC_M      , KC_COMM, KC_DOT , KC_SLSH  , MT_RBRC , 
-	                   S_DOWN  , S_UP    , S_HOME    , S_SPC   , KC_ENT,      SYMR    , SFT_SPC    , S_END     , S_LEFT , S_RIGHT,  
-	                                       KC_LCTL   , KC_BSPC ,                        SSYMR      , S_ESC 
+        KC_GRV  , KC_Q   , KC_W    , KC_E    , KC_R      , KC_T                           , KC_Y       , KC_U      , KC_I   , KC_O   , KC_P     , KC_LBRC ,
+        KC_DEL  , KC_A   , KC_S    , KC_D    , KC_F      , KC_G                           , KC_H       , KC_J      , KC_K   , KC_L   , KC_SCLN  , KC_QUOT ,
+        MT_TAB  , KC_Z   , KC_X    , KC_C    , KC_V      , KC_B                           , KC_N       , KC_M      , KC_COMM, KC_DOT , KC_SLSH  , MT_RBRC ,
+	                       S_DOWN  , S_UP    , S_HOME    , S_SPC   , KC_ENT,      SYMR    , SFT_SPC    , S_END     , S_LEFT , S_RIGHT,
+	                                           KC_LCTL   , KC_BSPC ,                        SSYMR      , S_ESC
 	),
 
 	[_SYMR] = KEYMAP(
-	_______, KC_2   , KC_3   , KC_4   , KC_5   , KC_6                                 , KC_7      , KC_8    , KC_9     , KC_0   , KC_MINS, KC_BSPC, 
-      KC_DEL , KC_1   , KC_NUBS, KC_LPRN, KC_RPRN, KC_BSLS                              , KC_LEFT   , KC_DOWN , KC_UP    , KC_RGHT, KC_EQL , KC_ENT ,
-      MT_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6                                , KC_F7     , KC_F8   , KC_F9    , KC_F10 , KC_F11 , MT_F12 , 
-                        _______, _______, _______, _______ , _______,           _______ , _______   , RESET   , KC_BRK   , KC_INS ,  
-		                              _______, _______ ,                              _______   , KC_LSFT 
+        _______, KC_2   , KC_3   , KC_4   , KC_5   , KC_6                                 , KC_7      , KC_8    , KC_9     , KC_0   , KC_MINS, KC_BSPC,
+        KC_DEL , KC_1   , KC_NUBS, KC_LPRN, KC_RPRN, KC_BSLS                              , KC_LEFT   , KC_DOWN , KC_UP    , KC_RGHT, KC_EQL , KC_ENT ,
+        MT_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6                                , KC_F7     , KC_F8   , KC_F9    , KC_F10 , KC_F11 , MT_F12 ,
+                          _______, _______, _______, _______ , _______,           _______ , _______   , RESET   , KC_BRK   , KC_INS ,
+		                                    _______, _______ ,                              _______   , KC_LSFT
 	),
 
 	[_SYML] = KEYMAP(
-	TO(_NUM), KC_MUTE , KC_VOLD , KC_UP   , KC_VOLU    , KC_PGUP                          , _______, KC_BTN1 , KC_BTN2 , _______ , KC_WH_U , KC_BSPC ,  
-      _______ , KC_PSCR , KC_LEFT , KC_DOWN , KC_RGHT    , KC_PGDN                          , KC_MS_L, KC_MS_D , KC_MS_U , KC_MS_R , KC_WH_D , KC_ENT  ,  
-      MT_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5      , KC_F6                            , KC_F7  , KC_F8   , KC_F9   , KC_F10  , KC_F11  , MT_F12  , 
-	                    KC_LALT , KC_LWIN , TO(_MEDIA) , _______, KC_SLEP,        KC_BTN1 , _______, _______ , _______ , _______ ,  
-	                                        _______    , _______,                           KC_BTN2, _______ 
+        _______, KC_MUTE , KC_VOLD , KC_UP   , KC_VOLU    , KC_PGUP                          , _______, KC_BTN1 , KC_BTN2 , _______ , KC_WH_U , KC_BSPC ,
+        _______, KC_PSCR , KC_LEFT , KC_DOWN , KC_RGHT    , KC_PGDN                          , KC_MS_L, KC_MS_D , KC_MS_U , KC_MS_R , KC_WH_D , KC_ENT  ,
+        MT_F1  , KC_F2   , KC_F3   , KC_F4   , KC_F5      , KC_F6                            , KC_F7  , KC_F8   , KC_F9   , KC_F10  , KC_F11  , MT_F12  ,
+	                       KC_LALT , KC_LWIN , TO(_MEDIA) , _______, KC_SLEP,        KC_BTN1 , _______, _______ , _______ , _______ ,
+	                                           _______    , _______,                           KC_BTN2, _______
 	),
-
-
-	[_NUM] = KEYMAP(
-	TO(_QWERTY), _______ , _______ , _______ , _______ , _______                          , KC_P7   , KC_P8   , KC_P9   , KC_PSLS , KC_PAST , KC_BSPC ,
-      _______    , _______ , _______ , _______ , _______ , _______                          , KC_P4   , KC_P5   , KC_P6   , KC_PPLS , KC_PMNS , KC_ENT  ,
-      _______    , _______ , _______ , _______ , _______ , _______                          , KC_P1   , KC_P2   , KC_P3   , KC_P0   , KC_PDOT , KC_PENT ,
-	                       _______ , _______ , _______ , _______, _______,        _______ , _______ , KC_NUM  , _______ , _______ ,  
-	                                           _______ , _______,                           _______, _______ 
-	),
-
 
 	[_MEDIA] = KEYMAP(
-	_______ , KC_MUTE , KC_VOLD , KC_UP   , KC_VOLU     , KC_PGUP                          , _______, KC_BTN1 , KC_BTN2 , _______ , KC_WH_U , _______ ,  
-      _______ , C(KC_W) , KC_LEFT , KC_DOWN , KC_RGHT     , KC_PGDN                          , KC_MS_L, KC_MS_D , KC_MS_U , KC_MS_R , KC_WH_D , _______ ,  
-      _______ , _______ , _______ , _______ , _______     , _______                          , _______, _______ , _______ , _______ , _______ , _______ , 
-	                    _______ , _______ , TO(_QWERTY) , _______, _______,        KC_LEFT , KC_RGHT, _______ , KC_BTN1 , KC_BTN2 ,  
-	                                        _______     , _______,                           KC_DOWN, KC_UP 
+        _______ , KC_MUTE , KC_VOLD , KC_UP   , KC_VOLU     , KC_PGUP                          , _______, KC_BTN1 , KC_BTN2 , _______ , KC_WH_U , _______ ,
+        _______ , C(KC_W) , KC_LEFT , KC_DOWN , KC_RGHT     , KC_PGDN                          , KC_MS_L, KC_MS_D , KC_MS_U , KC_MS_R , KC_WH_D , _______ ,
+        _______ , _______ , _______ , _______ , _______     , _______                          , _______, _______ , _______ , _______ , _______ , _______ ,
+	                        _______ , _______ , TO(_QWERTY) , _______, _______,        KC_LEFT , KC_RGHT, _______ , KC_BTN1 , KC_BTN2 ,
+	                                            _______     , _______,                           KC_DOWN, KC_UP
 	),
 };
 
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
 	// define keys that aren't held (usually taped once)
-      switch (keycode) {
+    switch (keycode) {
 		case S_ESC:
-                  return true;	
-
 		case MT_TAB:
-                  return true;	
 		case MT_RBRC:
-                  return true;	
-            case MT_F1:
-                  return true;	
+        case MT_F1:
 		case MT_F12:
-                  return true;	
-
 		case S_HOME:
-                  return true;	
 		case S_END:
-                  return true;	
+            return true;
 
-            default:
-                  return false;
-      }
+        default:
+            return false;
+    }
 }
 
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
 	// define keys that should be handle as modifiers after pressing some other keys withing tap term
-      switch (keycode) {
+    switch (keycode) {
 		case S_SPC:
-                  return true;		
-            case SFT_SPC:
-                  return true;	
-		
+        case SFT_SPC:
+
 		case S_ESC:
-                  return true;	
-
 		case MT_TAB:
-                  return true;	
 		case MT_RBRC:
-                  return true;	
-            case MT_F1:
-                  return true;	
-            case MT_F12:
-                  return true;	
-
+        case MT_F1:
+        case MT_F12:
 		case S_HOME:
-                  return true;	
 		case S_END:
-                  return true;	
-	
-		case S_UP:
-                  return true;	
-		case S_DOWN:
-                  return true;	
-		case S_LEFT:
-                  return true;	
-		case S_RIGHT:
-                  return true;	
 
-            default:
-                  return false;
-      }
-} 
+		case S_UP:
+		case S_DOWN:
+		case S_LEFT:
+		case S_RIGHT:
+            return true;
+
+        default:
+            return false;
+    }
+}
 
 bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
-      switch (keycode) {
+    switch (keycode) {
 		case S_ESC:
-                  return true;	
-
 		case MT_TAB:
-                  return true;	
 		case MT_RBRC:
-                  return true;	
-            case MT_F1:
-                  return true;	
+        case MT_F1:
 		case MT_F12:
-                  return true;	
-
 		case S_HOME:
-                  return true;	
 		case S_END:
-                  return true;	
+            return true;
 
-            default:
-                  return false;
+        default:
+            return false;
     }
 }
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
-      switch (keycode) {
-            case MT_TAB:
-                  return true;	
-            case MT_RBRC:
-                  return true;	
-            case MT_F1:
-                  return true;	
-            case MT_F12:
-                  return true;	
+    switch (keycode) {
+        case MT_TAB:
+        case MT_RBRC:
+        case MT_F1:
+        case MT_F12:
+            return true;
 
-            default:
-                  // Do not select the hold action when another key is pressed.
-                  return false;
-      }
+        default:
+            // Do not select the hold action when another key is pressed.
+            return false;
+    }
 }
 
 #ifdef RGBLIGHT_ENABLE
@@ -203,21 +159,21 @@ const rgblight_segment_t PROGMEM gui_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     ctl_layer,
-    alt_layer,   
+    alt_layer,
     sft_layer,
     gui_layer
 );
 
 void keyboard_post_init_user(void) {
-	rgblight_disable_noeeprom();	
+	rgblight_disable_noeeprom();
 	rgblight_sethsv_noeeprom(0, 0, 0);
 	rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-      rgblight_disable_noeeprom();	
-	
+    rgblight_disable_noeeprom();
+
 	rgblight_enable_noeeprom();
 	rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
 	rgblight_sethsv_noeeprom(0, 0, 0);
-	
+
 	rgblight_layers = my_rgb_layers;
 }
 
@@ -228,39 +184,35 @@ void update_mod_layers(uint16_t mods) {
 	rgblight_set_layer_state(3, mods & MOD_MASK_GUI);
 }
 
-void post_process_record_user(uint16_t keycode, keyrecord_t *record) {	
+void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
 	update_mod_layers(get_mods() | get_oneshot_mods());
-}	
+}
 
 uint32_t layer_state_set_user(uint32_t state) {
-	int highest_layer = get_highest_layer(state); 
+	int highest_layer = get_highest_layer(state);
 
-      rgblight_enable_noeeprom();
-      switch (highest_layer) {
-            case _QWERTY:
-		      rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-			rgblight_sethsv_noeeprom(0, 0, 0);
-                  break;
-            case _SYML:
-                  rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_GRADIENT + 3);
-			rgblight_sethsv_noeeprom(170, 255, 170);
-                  break;
-            case _SYMR:
-                  rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_GRADIENT + 7);
-			rgblight_sethsv_noeeprom(170, 255, 170);
-                  break;
-            
-            case _MEDIA: 
-                  rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING);
-                        rgblight_sethsv_noeeprom(170, 255, 170);
-                  break;
-            case _NUM: 
-                  rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING);
-                        rgblight_sethsv_noeeprom(64, 255, 170);
-                  break;	
+    rgblight_enable_noeeprom();
+    switch (highest_layer) {
+        case _QWERTY:
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+            rgblight_sethsv_noeeprom(0, 0, 0);
+            break;
+        case _SYML:
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_GRADIENT + 3);
+            rgblight_sethsv_noeeprom(170, 255, 170);
+            break;
+        case _SYMR:
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_GRADIENT + 7);
+            rgblight_sethsv_noeeprom(170, 255, 170);
+            break;
+
+        case _MEDIA:
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING);
+            rgblight_sethsv_noeeprom(170, 255, 170);
+            break;
 	}
 
-      return state;
+    return state;
 }
 
 #endif // RGBLIGHT_ENABLE
--- keyboards/sofle/keymaps/jinoralen/keymap.c
+++ keyboards/sofle/keymaps/jinoralen/keymap.c
@@ -11,6 +11,7 @@ enum sofle_layers {
 #define SSYMR   LM(_SYMR, MOD_LSFT)
 
 #define S_SPC   LT(_SYML, KC_SPC)
+#define SFT_ESC SFT_T(KC_ESC)
 
 #define S_UP    MT(MOD_LGUI, KC_UP)
 #define S_DOWN  MT(MOD_LALT, KC_DOWN)
@@ -27,25 +28,23 @@ enum sofle_layers {
 #define MT_RBRC MT(MOD_LSFT, KC_RBRC)
 #define MT_F1   MT(MOD_LSFT, KC_F1)
 #define MT_F12  MT(MOD_LSFT, KC_F12)
-#define MT_F2   MT(MOD_LCTL, KC_F2)
-#define MT_F11  MT(MOD_LCTL, KC_F11)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
 //    KC_GRV  , KC_2   , KC_3    , KC_4    , KC_5      , KC_6                           , KC_7       , KC_8    , KC_9   , KC_0   , KC_MINS  , KC_EQL  , 
     	XXXXXXX , XXXXXXX, XXXXXXX , XXXXXXX , XXXXXXX   , XXXXXXX                        , XXXXXXX    , XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX  , XXXXXXX , 
-	KC_ESC  , KC_Q   , KC_W    , KC_E    , KC_R      , KC_T                           , KC_Y       , KC_U    , KC_I   , KC_O   , KC_P     , KC_LBRC , 
+	KC_GRV  , KC_Q   , KC_W    , KC_E    , KC_R      , KC_T                           , KC_Y       , KC_U    , KC_I   , KC_O   , KC_P     , KC_LBRC , 
       KC_DEL  , KC_A   , KC_S    , KC_D    , KC_F      , KC_G                           , KC_H       , KC_J    , KC_K   , KC_L   , KC_SCLN  , KC_QUOT , 
       MT_TAB  , KC_Z   , KC_X    , KC_C    , KC_V      , KC_B    , KC_MUTE    , RESET   , KC_N       , KC_M    , KC_COMM, KC_DOT , KC_SLSH  , MT_RBRC , 
-	                   S_DOWN  , S_UP    , S_HOME    , KC_LCTL , S_SPC      , SYMR    , KC_LSFT    , S_END   , S_LEFT , S_RIGHT  
+	                   S_DOWN  , S_UP    , S_HOME    , KC_LCTL , S_SPC      , SYMR    , SFT_ESC    , S_END   , S_LEFT , S_RIGHT  
 	),
 
 	[_SYMR] = LAYOUT(
 	_______, _______, _______, _______, _______, _______ ,                              _______   , _______ , _______  , _______, _______, _______, 
-      _______, KC_GRV , KC_3   , KC_4   , KC_5   , KC_6    ,                              KC_7      , KC_8    , KC_9     , KC_0   , KC_MINS, KC_BSPC, 
-	KC_DEL , KC_1   , KC_2   , KC_LPRN, KC_RPRN, KC_BSLS ,                              KC_LEFT   , KC_DOWN , KC_UP    , KC_RGHT, KC_EQL , KC_ENT , 
-	MT_F1  , MT_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6   , _______          , _______  ,KC_F7     , KC_F8   , KC_F9    , KC_F10 , MT_F11 , MT_F12 , 
+      _______, KC_2   , KC_3   , KC_4   , KC_5   , KC_6    ,                              KC_7      , KC_8    , KC_9     , KC_0   , KC_MINS, KC_BSPC, 
+	KC_DEL , KC_1   , KC_NUBS, KC_LPRN, KC_RPRN, KC_BSLS ,                              KC_LEFT   , KC_DOWN , KC_UP    , KC_RGHT, KC_EQL , KC_ENT , 
+	MT_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6   , _______          , _______  ,KC_F7     , KC_F8   , KC_F9    , KC_F10 , KC_F11 , MT_F12 , 
 	                  _______, _______, _______, _______ , _______          , _______  ,_______   , _______ , KC_BRK   , KC_INS
 
 	),
@@ -54,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______ , _______ , _______ , _______ , _______ , _______ ,                        _______, _______ , _______ , _______ , _______ , _______ , 
 	_______ , KC_MUTE , KC_VOLD , KC_UP   , KC_VOLU , KC_PGUP ,                        _______, KC_BTN1 , KC_BTN2 , _______ , KC_WH_U , KC_BSPC ,  
 	KC_BSPC , KC_PSCR , KC_LEFT , KC_DOWN , KC_RGHT , KC_PGDN ,                        KC_MS_L, KC_MS_D , KC_MS_U , KC_MS_R , KC_WH_D , KC_ENT  , 
-	MT_F1   , MT_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , _______     , _______ ,KC_F7  , KC_F8   , KC_F9   , KC_F10  , MT_F11  , MT_F12  , 
+	MT_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , _______     , _______ ,KC_F7  , KC_F8   , KC_F9   , KC_F10  , KC_F11  , MT_F12  , 
 	                    KC_LALT , KC_LWIN , _______ , _______ , _______     , _______ ,_______, _______ , KC_HOME , KC_END   
 	), 
 
@@ -62,106 +61,65 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______ , _______ , _______ , _______ , _______ , _______ ,                        _______, _______ , _______ , _______ , _______ , _______ , 
 	_______ , KC_MUTE , KC_VOLD , KC_UP   , KC_VOLU , KC_PGUP ,                        _______, KC_BTN1 , KC_BTN2 , _______ , KC_WH_U , KC_BSPC ,  
 	KC_BSPC , KC_PSCR , KC_LEFT , KC_DOWN , KC_RGHT , KC_PGDN ,                        KC_MS_L, KC_MS_D , KC_MS_U , KC_MS_R , KC_WH_D , KC_ENT  , 
-	MT_F1   , MT_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , _______     , _______ ,KC_F7  , KC_F8   , KC_F9   , KC_F10  , MT_F11  , MT_F12  , 
+	MT_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , _______     , _______ ,KC_F7  , KC_F8   , KC_F9   , KC_F10  , KC_F11  , MT_F12  , 
 	                    KC_LALT , KC_LWIN , _______ , _______ , _______     , _______ ,_______, _______ , KC_HOME , KC_END   
 	), 
 };
 
 
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
-	// define keys that aren't held (usually taped once)
+      // define keys that aren't held (usually taped once)
       switch (keycode) {
 		case S_ESC:
-                  return true;	
 		case MT_TAB:
-                  return true;	
-		case MT_RBRC:
-                  return true;	
-
+            case MT_RBRC:
             case MT_F1:
-                  return true;	
 		case MT_F12:
-                  return true;	
-            case MT_F2:
-                  return true;	
-		case MT_F11:
-                  return true;	
-
 		case S_HOME:
-                  return true;	
 		case S_END:
-                  return true;	
+                  return true;
 
             default:
                   return false;
-    }
+      }
 }
 
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
 	// define keys that should be handle as modifiers after pressing some other keys withing tap term
       switch (keycode) {
 		case S_SPC:
-                  return true;		
-		
+            case SFT_ESC:
+
 		case S_ESC:
-                  return true;	
-
 		case MT_TAB:
-                  return true;	
 		case MT_RBRC:
-                  return true;	
             case MT_F1:
-                  return true;	
-		case MT_F12:
-                  return true;	
-            case MT_F2:
-                  return true;	
-		case MT_F11:
-                  return true;	
-
-
+            case MT_F12:
 		case S_HOME:
-                  return true;	
 		case S_END:
-                  return true;	
-	
+
 		case S_UP:
-                  return true;	
 		case S_DOWN:
-                  return true;	
 		case S_LEFT:
-                  return true;	
 		case S_RIGHT:
-                  return true;	
+                  return true;
 
             default:
                   return false;
-      }
+      }      
 } 
+
 
 bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
       switch (keycode) {
 		case S_ESC:
-                  return true;	
-
 		case MT_TAB:
-                  return true;	
 		case MT_RBRC:
-                  return true;	
             case MT_F1:
-                  return true;	
 		case MT_F12:
-                  return true;	 
-            case MT_F2:
-                  return true;	
-		case MT_F11:
-                  return true;	
-
-
 		case S_HOME:
-                  return true;	
 		case S_END:
-                  return true;	
+                  return true;
 
             default:
                   return false;
@@ -169,24 +127,17 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
 }
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        	case MT_TAB:
-                  return true;	
-		case MT_RBRC:
-                  return true;	
+      switch (keycode) {
+            case MT_TAB:
+            case MT_RBRC:
             case MT_F1:
-                  return true;	
-		case MT_F12:
-                  return true;	
-            case MT_F2:
-                  return true;	
-		case MT_F11:
-                  return true;	
+            case MT_F12:
+                  return true;
 
-        default:
-            // Do not select the hold action when another key is pressed.
-            return false;
-    }
+            default:
+                  // Do not select the hold action when another key is pressed.
+                  return false;
+      }
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
